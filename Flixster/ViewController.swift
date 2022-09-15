3bb00d37954a6a19261e4692020d7d5c29ebb517
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2022-09-14 23:29:34 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Sagar Rathod on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
