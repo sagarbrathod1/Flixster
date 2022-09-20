f3d5fb23b5515a113e7584d5f4a67def298aa656
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2022-09-20 02:39:38 -0400

    part 2 additions

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Sagar Rathod on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+
+}
